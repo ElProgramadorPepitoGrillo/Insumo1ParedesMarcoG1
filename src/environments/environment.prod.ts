3d4b877ed5c4e3eff6bd7352cffbaa86935d5e9b
S3BASCH <sebaschuquiguanga@gmail.com>
2023-04-17 10:49:09 -0500

    as

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+   firebaseConfig : {
+    apiKey: "AIzaSyC56wFOw2DzuVjvVNUSPU7ZUci4deppa34",
+    authDomain: "insumo1-marcoparedes-g2.firebaseapp.com",
+    projectId: "insumo1-marcoparedes-g2",
+    storageBucket: "insumo1-marcoparedes-g2.appspot.com",
+    messagingSenderId: "729975487986",
+    appId: "1:729975487986:web:40eee127601c6bf3ddff6b"
+  }
+};
